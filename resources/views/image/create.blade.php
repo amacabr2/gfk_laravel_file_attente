4f9841132aba4d72a4922794d308dedaf9f1b1d2
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-17 11:16:55 +0200

    Creation of the form to register images

---
+++ resources/views/image/create.blade.php
@@ -0,0 +1,18 @@
+@yield('base')
+
+@section('body')
+
+    <h1>Uploader une image</h1>
+
+    <form action="/" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="file">Fichier image</label>
+            <input type="file" class="form-control" id="file">
+        </div>
+        <div class="form-control">
+            <button class="btn btn-primary">Envoyer l'image</button>
+        </div>
+    </form>
+
+@endsection
