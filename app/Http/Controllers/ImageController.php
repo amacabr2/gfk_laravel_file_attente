1a0326b1de47dbffb15567b239e6d0d5389c1272
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-17 11:08:44 +0200

    Creation of the controller ImageController with two actions. The first one creates the template with the form to add embellishes with images them and the second for the treatment of the form

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller {
+
+    public function create() {
+        return view('image.create');
+    }
+
+    public function store() {
+
+    }
+
+}
